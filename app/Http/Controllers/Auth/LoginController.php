05bfbff20060c7fdd6aebd03a930c5cc8806b198
akpanemmanueledidiong99@gmail.com <akpanemmanueledidiong99@gmail.com>
2022-08-05 15:25:30 +0100

    new changes

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -54,64 +54,67 @@ class LoginController extends Controller
 
     public function Authenticated (Request $request, User $user)
     {
+        Auth::logoutOtherDevices($request->password);
 
-        if(Auth::check()) {
+        return redirect()->intended();
 
-            $user = Auth::user();
+        // if(Auth::check()) {
 
-            Session::put('join', 1);
-            Session::put('welcome', 1);
+        //     $user = Auth::user();
 
-            if(!empty($user->license_code)) {
-                $license = LicensedUserSession::where('user_id', $user->id)->first();
+        //     Session::put('join', 1);
+        //     Session::put('welcome', 1);
 
-                if($license) {
-                    $license_count = $license->count();
-                    $licensed_users = License::where('license_code', $user->license_code)->first();
-                    $allowed_users = $licensed_users->active_users + 1;
-                    if($license_count < $allowed_users) {
-                        // $license->increment('session_no');
-                        LicensedUserSession::create([
-                            'license_id' => $license->id,
-                            'user_id' => $user->id,
-                            'session_no' => time(),
-                        ]);
+        //     if(!empty($user->license_code)) {
+        //         $license = LicensedUserSession::where('user_id', $user->id)->first();
 
-                    } if($license_count >= $allowed_users) {
+        //         if($license) {
+        //             $license_count = $license->count();
+        //             $licensed_users = License::where('license_code', $user->license_code)->first();
+        //             $allowed_users = $licensed_users->active_users + 1;
+        //             if($license_count < $allowed_users) {
+        //                 // $license->increment('session_no');
+        //                 LicensedUserSession::create([
+        //                     'license_id' => $license->id,
+        //                     'user_id' => $user->id,
+        //                     'session_no' => time(),
+        //                 ]);
 
-                        foreach($license as $login) {
-                            // dd($login);
-                            if ($login->session_no < time() - 300) {
-                                $logged_out_user = LicensedUserSession::where('id', $login->id)->orderBy('session_no', 'ASC')->first();
-                                $logged_out_user->delete();
-                                $request->session()->flush();
-                                Auth::logout();
-                                Session::flash('error', 'Time expired');
-                                return redirect('/login')->withErrors('Time expired');
-                            }
-                        }
-                        Auth::logout();
-                        Session::flash('error', 'Maximmum number of users reached');
-                        return redirect('/login')->withErrors('Maximmum number of users reached');
-                    }
-                } else {
-                    $license = License::where('license_code', $user->license_code)->first();
-                    LicensedUserSession::create([
-                        'license_id' => $license->id,
-                        'user_id' => $user->id,
-                        'session_no' => time(),
-                    ]);
+        //             } if($license_count >= $allowed_users) {
 
-                    return redirect()->intended();
+        //                 foreach($license as $login) {
+        //                     // dd($login);
+        //                     if ($login->session_no < time() - 300) {
+        //                         $logged_out_user = LicensedUserSession::where('id', $login->id)->orderBy('session_no', 'ASC')->first();
+        //                         $logged_out_user->delete();
+        //                         $request->session()->flush();
+        //                         Auth::logout();
+        //                         Session::flash('error', 'Time expired');
+        //                         return redirect('/login')->withErrors('Time expired');
+        //                     }
+        //                 }
+        //                 Auth::logout();
+        //                 Session::flash('error', 'Maximmum number of users reached');
+        //                 return redirect('/login')->withErrors('Maximmum number of users reached');
+        //             }
+        //         } else {
+        //             $license = License::where('license_code', $user->license_code)->first();
+        //             LicensedUserSession::create([
+        //                 'license_id' => $license->id,
+        //                 'user_id' => $user->id,
+        //                 'session_no' => time(),
+        //             ]);
 
-                }
-            }
+        //             return redirect()->intended();
+
+        //         }
+        //     }
 
 
-            Auth::logoutOtherDevices($request->password);
+        //     Auth::user()->logoutOtherDevices($request->password);
 
-            return redirect()->intended();
-        }
+        //     return redirect()->intended();
+        // }
     }
 
 }
