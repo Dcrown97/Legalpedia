ef888fb0aa7272c15f6eb156ca430730d33a3be7
Adekoya Adebayo <adekoya.adebayojubril@gmail.com>
2023-08-21 18:07:57 +0100

    add type

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -152,6 +152,7 @@ class AdminController extends Controller
             $result_title = 'AI Analysis Result';
             return view('admin.ai_assistant_result', compact('summary', 'teams', 'result_title'));
         } catch (\Exception $e) {
+            dd($e);
             return back()->withErrors('An error occurred');
         }
     }
