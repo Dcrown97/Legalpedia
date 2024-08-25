3ce2193dfd49a740064d2c5d59230303cf736fbc
Aisimiyu Oluwadarasimi <aisimiyuoluwadara@gmail.com>
2024-08-25 21:27:22 +0100

    updated package migrations

--- database/migrations/old/2021_12_17_130558_create_packages_table.php
+++ database/migrations/old/2021_12_17_130558_create_packages_table.php
@@ -32,20 +32,6 @@ class CreatePackagesTable extends Migration
             $table->text('dict_feature')->nullable();
             $table->text('resource_feature')->nullable();
             $table->string('slug')->nullable();
-            $table->string('api_access')->nullable();
-            $table->string('postman_link')->nullable();
-
-            $table->text('judgement_featureapi')->nullable();
-            $table->text('lfn_featureapi')->nullable();
-            $table->text('roc_featureapi')->nullable();
-            $table->text('sroc_featureapi')->nullable();
-            $table->text('form_featureapi')->nullable();
-            $table->text('article_featureapi')->nullable();
-            $table->text('maxim_featureapi')->nullable();
-            $table->text('dict_featureapi')->nullable();
-            $table->text('resource_featureapi')->nullable();
-            $table->text('ai_featureapi')->nullable();
-
             $table->timestamps();
         });
     }
