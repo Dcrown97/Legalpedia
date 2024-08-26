55829d09818fd643280ca2c2b5efd296de44d2f2
Aisimiyu Oluwadarasimi <aisimiyuoluwadara@gmail.com>
2024-08-26 17:55:54 +0100

    routes updated

--- routes/api.php
+++ routes/api.php
@@ -44,14 +44,14 @@ Route::group(['middleware' => ['cors', 'json.response', 'XSS']], function () {
     // public routes
     Route::post('/register', [ApiRegisterController::class, "register"]);
     Route::post('/login', [ApiLoginController::class, "login"]);
-    // Route::get('/admin/all-judgements', [ApiAdminController::class, 'allJudgement'])->name('admin.judgement');
-    Route::get('/admin/all-judgements', [ApiAdminController::class, 'allJudgement'])->name('admin.judgement');
-    Route::get('/admin/judgements/allsubject-matter', [ApiAdminController::class, 'allSbjMatter'])->name('judgement.sbj-matter');
-    Route::get('/admin/judgements/all-legal-citation', [ApiAdminController::class, 'allLegalCitation'])->name('judgement.citation');
-    Route::get('/admin/judgements/subject-matter-indices', [ApiAdminController::class, 'subject_matter_indices'])->name('judgement.sbj-matter');
-    Route::get('/admin/all-rules-of-court', [ApiAdminController::class, 'allRules'])->name('admin.rules-of-court');
-    Route::get('/admin/all-legal-maxims', [ApiAdminController::class, 'allMaxim'])->name('admin.legal-maxims');
-    Route::get('/admin/all-law-dictionary', [ApiAdmincontroller::class, 'allDictionary'])->name('admin.law-dictionary');
+    // Route::get('/admin/all-judgements', [ApiAdminController::class, 'allJudgement']);
+    Route::get('/admin/all-judgements', [ApiAdminController::class, 'allJudgement']);
+    Route::get('/admin/judgements/allsubject-matter', [ApiAdminController::class, 'allSbjMatter']);
+    Route::get('/admin/judgements/all-legal-citation', [ApiAdminController::class, 'allLegalCitation']);
+    Route::get('/admin/judgements/subject-matter-indices', [ApiAdminController::class, 'subject_matter_indices']);
+    Route::get('/admin/all-rules-of-court', [ApiAdminController::class, 'allRules']);
+    Route::get('/admin/all-legal-maxims', [ApiAdminController::class, 'allMaxim']);
+    Route::get('/admin/all-law-dictionary', [ApiAdmincontroller::class, 'allDictionary']);
     Route::get('/admin/all-law-of-fed', [ApiAdmincontroller::class, 'allFed']);
     Route::get('/admin/all-forms', [ApiAdmincontroller::class, 'allForms']);
     Route::get('/admin/all-rule-categories', [ApiAdmincontroller::class, 'allRuleCategorie']);
